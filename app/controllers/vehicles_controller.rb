7d620e67e57748be49c2aee832b73bd66d61355e
ShaoEnChen <seanchen47@gmail.com>
2018-10-23 18:19:19 -0500

    fix new&edit model attribute failure

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,6 +1,12 @@
 class VehiclesController < ApplicationController
+	before_action :get_vehicle, :only => [:show, :edit, :update, :destroy]
+
+	def get_vehicle
+		@vehicle = Vehicle.find params[:id]
+	end
+
 	def vehicle_params
-		params.permit(:name)
+		params.require(:vehicle).permit(:name)
 	end
 
 	def index
@@ -8,32 +14,27 @@ class VehiclesController < ApplicationController
 	end
 
 	def new
-		# default: render 'new' template
 	end
 
 	def show
-		@vehicle = Vehicle.find params[:id]
 	end
 
 	def create
-		@vehicle = Vehicle.create! vehicle_params
+		@vehicle = Vehicle.create!(vehicle_params)
 		flash[:notice] = "Vehicle #{@vehicle.id} was successfully created."
 		redirect_to vehicles_path
 	end
 
 	def edit
-		@vehicle = Vehicle.find params[:id]
 	end
 
 	def update
-		@vehicle = Vehicle.find params[:id]
-		@vehicle.update_attributes! vehicle_params
+		@vehicle.update_attributes!(vehicle_params)
 		flash[:notice] = "Vehicle #{@vehicle.id} was successfully updated."
 		redirect_to vehicles_path
 	end
 
 	def destroy
-		@vehicle = Vehicle.find params[:id]
 		@vehicle.destroy
 		flash[:notice] = "Vehicle #{@vehicle.id} was successfully deleted."
 		redirect_to vehicles_path
