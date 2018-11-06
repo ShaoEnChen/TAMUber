bc2b1698a08b9471e95da1d5cdc7e008a77a12cc
ShaoEnChen <seanchen47@gmail.com>
2018-11-06 12:46:33 -0600

    install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-	resources :vehicles, :administrators
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	#root 'vehicles#index'
-	#root :to => redirect('/vehicles')
-	
-	#resources :homepages
-	#root :to => redirect('/homepage')
 	root  'homepage#index'
+	resources :vehicles, :administrators
 end
