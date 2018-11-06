967289628d0f1c4c6105d7bd236b279ffd889ce5
簡均容 <amychien@Amys-MacBook-Pro.local>
2018-11-05 22:42:22 -0600

    add migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_201614) do
-
-  create_table "vehicles", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2018_10_31_201614) do
 
   create_table "administrators", force: :cascade do |t|
     t.string "name"
     t.string "account"
     t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
