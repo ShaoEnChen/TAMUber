7024af1c0181f399e4dd77c42a55d6ebe8b51630
JoeChou <yyyjoe94@gmail.com>
2018-11-07 16:41:43 -0600

    rspec test of three db

--- spec/controllers/user_spec.rb
+++ spec/controllers/user_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-    before(:all) do
-        @deiver_1 = User.create(name: "Yimin")
-        @deiver_2 = User.create(name: "Chou")
-    end
 
     it "#index" do
         login_with ( :user )
