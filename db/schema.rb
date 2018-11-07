7fcf401ac6ae98b1fa5f57b3b2d52c7bb8835a5a
ShaoEnChen <seanchen47@gmail.com>
2018-11-06 18:03:44 -0600

    fix order of schema to make a little more clear

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2018_11_06_205751) do
-  create_table "drivers", force: :cascade do |t|
+  create_table "vehicles", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2018_11_06_205751) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "vehicles", force: :cascade do |t|
+  create_table "drivers", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
