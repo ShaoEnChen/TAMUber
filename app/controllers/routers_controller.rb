9c1378a2b399a376af0f1811ba9730f72fa761ee
amy88328 <amy88328@hotmail.com>
2018-11-16 16:39:43 -0600

    add marker feature by controller

--- app/controllers/routers_controller.rb
+++ app/controllers/routers_controller.rb
@@ -1,9 +1,40 @@
 class RoutersController < ApplicationController
   before_action :set_router, only: [:show, :edit, :update, :destroy]
 
+  #Add load_routers and replaced index with the code below
+  def load_routers  
+   @routers_default = Gmaps4rails.build_markers(Router.all) do |plot, marker|  
+      marker.lat plot.latitude  
+      marker.lng plot.longitude  
+
+      @status = rand(1..4)  
+      @battery = rand(10..90)  
+      @ip = "192.168."+rand(0..255).to_s+"."+rand(15..250).to_s  
+      @connected = rand(50..100)  
+
+      if @status == 1  
+        url_alert = '/good.png'  
+        @status == "Normal"  
+      else  
+        url_alert = '/alert.png'  
+      end  
+
+      #marker.picture({  
+      #  "url" => url_alert,  
+      #  "width" => 35,  
+      #  "height" => 30  
+      #})  
+
+      marker.infowindow render_to_string(:partial => "/routers/info",   
+        :locals => {:name => plot.name, :battery => @battery, :date => rand(6.months.ago..Time.now), :ip => @ip, :connected => @connected })  
+   end  
+ end
+
   # GET /routers
   # GET /routers.json
   def index
+    load_routers  
+
     @routers = Router.all
   end
 
