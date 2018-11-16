a0dcda5ec6189567a8ef47a68aeec9635d02ef0f
amy88328 <amy88328@hotmail.com>
2018-11-16 15:51:58 -0600

    add google maps to router page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :routers
 	get 'users/index'
 	devise_for :users
 	root  'homepage#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_205751) do
+ActiveRecord::Schema.define(version: 2018_11_16_211017) do
 
   create_table "drivers", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "routers", force: :cascade do |t|
+    t.string "name"
+    t.float "latitude"
+    t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
